f6a6d538489f1f680e56a688c39be4a11eb59c7d
Radoy Stoyanov <radoy.stoyanov@gmail.com>
2023-10-16 18:20:30 +0300

    Initial icons

---
+++ app/icons/index.ts
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import IconProps from "./types";
+export type IconType = FC<IconProps>;
+
+export { default as X } from "./X";
+export { default as User } from "./User";
