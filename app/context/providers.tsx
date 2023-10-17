7bfffe767eecb31d88f715b2ccce92a6e89b4169
Radoy Stoyanov <radoy.stoyanov@gmail.com>
2023-10-17 15:55:36 +0300

    Integrate React Query & setup providers

--- app/context/providers.tsx
+++ app/context/providers.tsx
@@ -5,14 +5,20 @@ import RelayProvider from "./relay-context";
 import UserProvider from "./user-context";
 import KeysProvider from "./keys-context";
 import ToastProvider from "./toast-context";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers: FC<{ children: ReactNode }> = ({ children }) => (
   <RelayProvider>
-    <UserProvider>
-      <ToastProvider>
-        <KeysProvider>{children}</KeysProvider>
-      </ToastProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <ToastProvider>
+          <KeysProvider>{children}</KeysProvider>
+        </ToastProvider>
+      </UserProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </RelayProvider>
 );
 
---
+++ app/lib/queryClient.ts
@@ -0,0 +1,4 @@
+import { QueryClient } from "@tanstack/react-query";
+
+// TODO: Add some central logging through the QueryClient options
+export const queryClient = new QueryClient();
