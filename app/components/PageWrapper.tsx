3bfb37b806ff4cac58358fab98c50ed7935d7528
Radoy Stoyanov <radoy.stoyanov@gmail.com>
2023-10-18 23:36:31 +0300

    PageWrapper component

---
+++ app/components/PageWrapper.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useContext, useEffect } from "react";
+import { UserContext } from "@/context/user-context";
+import { useRouter } from "next/navigation";
+import FullPageLoader from "./FullPageLoader";
+
+interface PageWrapperProps {
+  children: React.ReactNode;
+}
+
+const PageWrapper: React.FC<PageWrapperProps> = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { isUserLoading, isAuthenticated } = useContext(UserContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isUserLoading && !isAuthenticated) router.push("/");
+  }, [isUserLoading, isAuthenticated, router]);
+
+  return (
+    <div>
+      {isUserLoading ? <FullPageLoader /> : isAuthenticated && children}
+    </div>
+  );
+};
+
+export default PageWrapper;
