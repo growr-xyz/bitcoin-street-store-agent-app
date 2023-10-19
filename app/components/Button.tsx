92510510f5d1c0b4a71a79200984304dd0ac273b
Radoy Stoyanov <radoy.stoyanov@gmail.com>
2023-10-20 00:46:25 +0300

    Button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  // icon?: React.ReactNode;
+  href?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  href,
+  children,
+  className,
+  onClick,
+  ...rest
+}) => {
+  const buttonClassName = twMerge(
+    "min-w-fit rounded self-stretch sm:self-auto px-3 py-1 text-normal sm:px-6 sm:py-2 bg-orange-600 text-white sm:text-lg text-center whitespace-nowrap overflow-hidden overflow-ellipsis",
+    className
+  );
+
+  return href ? (
+    <Link className={buttonClassName} href={href}>
+      {children}
+    </Link>
+  ) : (
+    <button className={buttonClassName} onClick={onClick} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
