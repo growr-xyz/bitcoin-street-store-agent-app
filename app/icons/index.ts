eca562627bfb7cbe36f30d20dff7be0e6d574774
Radoy Stoyanov <radoy.stoyanov@gmail.com>
2023-10-23 13:59:46 +0300

    Restore Copy icon

--- app/icons/index.ts
+++ app/icons/index.ts
@@ -6,3 +6,4 @@ export { default as X } from "./X";
 export { default as User } from "./User";
 export { default as Back } from "./Back";
 export { default as Trash } from "./Trash";
+export { default as Copy } from "./Copy";
